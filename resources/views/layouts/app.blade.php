38ab940f0746810024127722c5692dcb1cb45fb8
ScupidC0des <11390074+GlgApr@users.noreply.github.com>
2021-05-29 13:18:53 +0700

    style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,7 @@
 
     <title>Store - Your Best Marketplace</title>
 
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    <link href="style/main.css" rel="stylesheet" />
-  </head>
+     </head>
 
   <body>
    <!-- Navbar -->
