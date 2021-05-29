0941ab7c12d7cfdac85133cd22d81ce2a1897f36
ScupidC0des <11390074+GlgApr@users.noreply.github.com>
2021-05-29 13:21:02 +0700

    sciprt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,21 +10,16 @@
     <meta name="author" content="" />
 
     <title>Store - Your Best Marketplace</title>
-
-     </head>
+<!-- Style  -->
+    </head>
 
   <body>
    <!-- Navbar -->
+
     <!-- Page Content -->
     
 <!-- Footer -->
-    <!-- Bootstrap core JavaScript -->
-    <script src="/vendor/jquery/jquery.slim.min.js"></script>
-    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script>
-      AOS.init();
-    </script>
-    <script src="/script/navbar-scroll.js"></script>
-  </body>
+
+<!-- Script  -->
+     </body>
 </html>
